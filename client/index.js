1bd277a527333796aa7a54cb6bdd0f5831865aca
densk1 <cardenk@gmail.com>
2018-05-29 23:47:30 +0100

    Fixed build server with express

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { render } from 'react-snapshot';
 import App from './App';
 import './index.css';
 
--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 // IMPORTS
 const express = require('express');
 const dotenv = require('dotenv');
-const React = require('react');
-const renderToString = require('react-dom/server');
 
 // TOOLS
 const cookieParser = require('cookie-parser');
@@ -17,8 +15,8 @@ dotenv.config();
 
 
 // APP SETUP
-app.use(express.static(path.join(__dirname, 'build')));
-app.use(cors);
+app.use(express.static(path.join(__dirname, '..', 'build')));
+// app.use(cors);
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -33,9 +31,9 @@ const PORT = process.env.PORT || 5000;
 
 app.all('/api', (req, res) => res.status(404).send('Api not yet implemented!'));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   if (process.env.NODE_ENV === 'production') {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
   } else {
     res.send(`Currently in ${process.env.NODE_ENV} mode. Build only served in production.`);
   }
