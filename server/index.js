999b4192d41d0e1438db8f7cb6a596f71d8e88ce
densk1 <>
2018-05-31 13:12:34 +0100

    Formating server/index.js

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json());
 
 
 // ENV VARIABLES
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 // console.log("process.env", process.env);
 
 // CORS
