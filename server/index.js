3fe4e172fa8c749e552edd439965fcdecb0c32b1
densk1 <cardenk@gmail.com>
2018-06-07 17:47:05 +0100

    Implemented transpile for App.js so it will be available for use with Forever in production

--- server/index.js
+++ server/index.js
@@ -3,9 +3,17 @@
 // IMPORTS
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('./config/cors');
+const chalk = require('chalk');
 
+let handleSSR;
 // SERVER SIDE RENDERING
-const handleSSR = require('./ssr/index.js');
+try {
+  handleSSR = require('./ssr/index.js');
+} catch (e) {
+  console.log(chalk.cyan(`PRODUCTION ONLY: ${chalk.inverse(e.message)}`));
+}
+
 const app = express();
 dotenv.config();
 
@@ -19,13 +27,13 @@ const path = require('path');
 
 // APP SETUP
 if (process.env.NODE_ENV === 'production') {
-  app.use('/static', express.static(path.join(__dirname, '../build/static')));
-  app.use('/img', express.static(path.join(__dirname, '../build/img')));
+  // js file
+  app.use('/static', express.static(path.join(__dirname, '../static')));
+  app.use('/img', express.static(path.join(__dirname, '../img')));
 } else {
-  const cors = require('./config/cors');
   app.use(cors);
 }
- 
+
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -36,15 +44,19 @@ app.use(bodyParser.json());
 // ENV VARIABLES
 const PORT = process.env.PORT || 5000;
 
-
+// API ROUTES
 app.all('/api', (req, res) => res.status(404).send('Api not yet implemented!'));
 
+// PAGE REQUESTS
+/**
+*   TODO
+*   1) Add middleware for login handler
+*/
 app.get('*', (req, res) => {
   if (process.env.NODE_ENV === 'production') {
     return handleSSR(req, res);
-  } else {
-    res.send(`Currently in ${process.env.NODE_ENV} mode. Build only served in production.`);
   }
+  return res.send(`Currently in ${process.env.NODE_ENV} mode. Build only served in production.`);
 });
 
 app.listen(PORT, () => console.log(`Running on port: ${PORT}`));
